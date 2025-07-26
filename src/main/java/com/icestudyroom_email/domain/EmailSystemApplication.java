eed446d748500694cc86be49a6b4537255c95944
glaxyt <glaxyt@naver.com>
2025-07-26 23:34:01 +0900

    fix: @EnableAsync 중복된 설정 제거

--- src/main/java/com/icestudyroom_email/domain/EmailSystemApplication.java
+++ src/main/java/com/icestudyroom_email/domain/EmailSystemApplication.java
@@ -3,10 +3,8 @@ package com.icestudyroom_email.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableKafka
-@EnableAsync
 @SpringBootApplication
 public class EmailSystemApplication {
 	public static void main(String[] args) {
