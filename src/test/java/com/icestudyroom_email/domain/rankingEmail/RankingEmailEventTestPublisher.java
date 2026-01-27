eb8d9ab5f9ceaa645085874acf537f3bade81ea1
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-27 15:02:01 +0900

    test : 랭킹 이메일 이벤트 로컬 테스트 코드 추가

---
+++ src/test/java/com/icestudyroom_email/domain/rankingEmail/RankingEmailEventTestPublisher.java
@@ -0,0 +1,29 @@
+package com.icestudyroom_email.domain.rankingEmail;
+
+import com.icestudyroom_email.domain.rankingContract.RankingEmailEvent;
+import com.icestudyroom_email.domain.rankingContract.RankingEventType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RankingEmailEventTestPublisher {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishTop5EnterEvent() {
+        RankingEmailEvent event = new RankingEmailEvent(
+                "123",
+                RankingEventType.TOP5_ENTER,
+                "테스트 유저 박다영",
+                "forTestRanking@gmail.com",
+                5,
+                6,
+                10);
+
+
+        kafkaTemplate.send("RANKING_EMAIL_EVENT", event);
+
+    }
+}
