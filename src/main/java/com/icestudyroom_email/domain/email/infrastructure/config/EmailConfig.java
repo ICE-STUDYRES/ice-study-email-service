3da1878899f9bf49013cdd559de755c6a0dcc248
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-30 12:39:30 +0900

    refactor: email infrastructure config 패키지 위치 정리

--- src/main/java/com/icestudyroom_email/domain/email/infrastructure/config/EmailConfig.java
+++ src/main/java/com/icestudyroom_email/domain/email/infrastructure/config/EmailConfig.java
@@ -1,4 +1,4 @@
-package com.icestudyroom_email.domain.config;
+package com.icestudyroom_email.domain.email.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
