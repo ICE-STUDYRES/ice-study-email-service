15f6dd151f40260e32ce84e6a5b03128488d2646
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-30 12:43:34 +0900

    rename: 메일/소켓 테스트 디렉토리 분리 및 import 정리

--- src/main/java/com/icestudyroom_email/domain/email/infrastructure/template/RankingEmailTemplateResolver.java
+++ src/main/java/com/icestudyroom_email/domain/email/infrastructure/template/RankingEmailTemplateResolver.java
@@ -1,7 +1,7 @@
 package com.icestudyroom_email.domain.email.infrastructure.template;
 
 import com.icestudyroom_email.domain.email.infrastructure.gmail.dto.EmailRequest;
-import com.icestudyroom_email.domain.rankingContract.RankingEmailEvent;
+import com.icestudyroom_email.domain.rankingContract.email.RankingEmailEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
