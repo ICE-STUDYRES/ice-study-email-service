24a104604cba7961dcb410d1a62534e11b38d489
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-30 12:40:23 +0900

    feat: kafka 프로파일에서만 RankingEmailConsumer 활성화(추가)

--- src/main/java/com/icestudyroom_email/domain/email/infrastructure/kafka/consumer/DltConsumer.java
+++ src/main/java/com/icestudyroom_email/domain/email/infrastructure/kafka/consumer/DltConsumer.java
@@ -2,6 +2,7 @@ package com.icestudyroom_email.domain.email.infrastructure.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("kafka")
 public class DltConsumer {
 
     @KafkaListener(topics = "vacancy-notifications.DLT", groupId = "dlt-group", containerFactory = "kafkaListenerContainerFactory")
