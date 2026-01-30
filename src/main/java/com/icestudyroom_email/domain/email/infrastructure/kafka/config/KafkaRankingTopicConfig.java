56fccb794a20bb8d259325a9fc3ce75ca91da446
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-30 12:38:16 +0900

    feat: kafka 프로파일에서만 RankingEmailConsumer 활성화(추가)

--- src/main/java/com/icestudyroom_email/domain/email/infrastructure/kafka/config/KafkaRankingTopicConfig.java
+++ src/main/java/com/icestudyroom_email/domain/email/infrastructure/kafka/config/KafkaRankingTopicConfig.java
@@ -1,11 +1,17 @@
 package com.icestudyroom_email.domain.email.infrastructure.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@ConditionalOnProperty(
+        name = "feature.kafka.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class KafkaRankingTopicConfig {
 
     @Bean
