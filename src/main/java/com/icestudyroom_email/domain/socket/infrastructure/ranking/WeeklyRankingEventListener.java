fe34c2f11e40e96d69c188af3fbab936bba4bbe2
Dayoung <pdayoung0402@hufs.ac.kr>
2026-01-30 13:52:14 +0900

    refactor: 사용되지 않는 WeeklyRankingItem DTO 삭제 및 import 수정

--- src/main/java/com/icestudyroom_email/domain/socket/infrastructure/ranking/WeeklyRankingEventListener.java
+++ src/main/java/com/icestudyroom_email/domain/socket/infrastructure/ranking/WeeklyRankingEventListener.java
@@ -1,10 +1,8 @@
 package com.icestudyroom_email.domain.socket.infrastructure.ranking;
 
 import com.icestudyroom_email.domain.rankingContract.socket.RankingListEvent;
-import com.icestudyroom_email.domain.rankingContract.socket.RankingPeriod;
 import com.icestudyroom_email.domain.rankingContract.socket.WeeklyRankingDto;
 import com.icestudyroom_email.domain.socket.infrastructure.protocol.ranking.RankingSocketEvent;
-import com.icestudyroom_email.domain.socket.infrastructure.ranking.dto.WeeklyRankingItem;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
